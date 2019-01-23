2a71027cf90e05f579745d0942c4e5da0ad9b997
Richard Dinh <richard.dinh@cfpb.gov>
2019-01-23 14:18:29 -0500

    update demo data

--- demos/src/demo-grouped-row.js
+++ demos/src/demo-grouped-row.js
@@ -12,80 +12,196 @@ let redrawCharts;
 
 require('./helpers/resizeHelper');
 
-const data = [
-    {
-        name: 'Credit card or prepaid card',
-        value: 200,
-        group: 'Sum of comparable companies',
-        striped: false
-    },
-    {
-        name: 'Credit card or prepaid card',
-        value: 700,
-        group: 'Average of comparable companies',
-        striped: true
-    },
-    {
-        name: 'Credit card or prepaid card',
-        value: 800,
-        group: 'EQUIFAX, INC.',
-        striped: false
-    },
-    {
-        name: 'Vehicle loan or lease',
-        value: 700,
-        group: 'Sum of comparable companies',
-        striped: false
-    },
-    {
-        name: 'Vehicle loan or lease',
-        value: 300,
-        group: 'Average of comparable companies',
-        striped: true
-    },
-    {
-        name: 'Vehicle loan or lease',
-        value: 300,
-        group: 'EQUIFAX, INC.',
-        striped: false
-    },
-    {
-        name: 'Debt collection',
-        value: 226,
-        group: 'Sum of comparable companies',
-        striped: false
-    },
-    {
-        name: 'Debt collection',
-        value: 750,
-        group: 'Average of comparable companies',
-        striped: true
-    },
-    {
-        name: 'Debt collection',
-        value: 960,
-        group: 'EQUIFAX, INC.',
-        striped: false
-    },
-    {
-        name: 'Credit reporting, credit repair services, or other personal consumer reports',
-        value: 16480,
-        group: 'Sum of comparable companies',
-        striped: false
-    },
-    {
-        name: 'Credit reporting, credit repair services, or other personal consumer reports',
-        value: 5000,
-        group: 'Average of comparable companies',
-        striped: true
-    },
-    {
-        name: 'Credit reporting, credit repair services, or other personal consumer reports',
-        value: 7500,
-        group: 'EQUIFAX, INC.',
-        striped: false
-    }
-];
+const data = [ {
+    "name": "Student loan",
+    "show": true,
+    "striped": false,
+    "readOnly": true,
+    "value": 0.2958579881656805,
+    "group": "Sum of comparable companies"
+}, {
+    "name": "Student loan",
+    "show": true,
+    "striped": true,
+    "readOnly": true,
+    "value": 0.29574132492113564,
+    "group": "Average of comparable companies"
+}, {
+    "name": "Student loan",
+    "readOnly": true,
+    "show": true,
+    "striped": false,
+    "value": 0.2985074626865672,
+    "group": "BANK OF AMERICA, NATIONAL ASSOCIATION"
+}, {
+    "name": "Payday loan, title loan, or personal loan",
+    "show": true,
+    "striped": false,
+    "readOnly": true,
+    "value": 0.5128205128205128,
+    "group": "Sum of comparable companies"
+}, {
+    "name": "Payday loan, title loan, or personal loan",
+    "show": true,
+    "striped": true,
+    "readOnly": true,
+    "value": 0.4100946372239748,
+    "group": "Average of comparable companies"
+}, {
+    "name": "Payday loan, title loan, or personal loan",
+    "readOnly": true,
+    "show": true,
+    "striped": false,
+    "value": 0.1492537313432836,
+    "group": "BANK OF AMERICA, NATIONAL ASSOCIATION"
+}, {
+    "name": "Vehicle loan or lease",
+    "show": true,
+    "striped": false,
+    "readOnly": true,
+    "value": 1.9526627218934909,
+    "group": "Sum of comparable companies"
+}, {
+    "name": "Vehicle loan or lease",
+    "show": true,
+    "striped": true,
+    "readOnly": true,
+    "value": 1.5615141955835963,
+    "group": "Average of comparable companies"
+}, {
+    "name": "Vehicle loan or lease",
+    "readOnly": true,
+    "show": true,
+    "striped": false,
+    "value": 0.9701492537313432,
+    "group": "BANK OF AMERICA, NATIONAL ASSOCIATION"
+}, {
+    "name": "Money transfer, virtual currency, or money service",
+    "show": true,
+    "striped": false,
+    "readOnly": true,
+    "value": 2.9980276134122286,
+    "group": "Sum of comparable companies"
+}, {
+    "name": "Money transfer, virtual currency, or money service",
+    "show": true,
+    "striped": true,
+    "readOnly": true,
+    "value": 2.397476340694006,
+    "group": "Average of comparable companies"
+}, {
+    "name": "Money transfer, virtual currency, or money service",
+    "readOnly": true,
+    "show": true,
+    "striped": false,
+    "value": 3.805970149253731,
+    "group": "BANK OF AMERICA, NATIONAL ASSOCIATION"
+}, {
+    "name": "Mortgage",
+    "show": true,
+    "striped": false,
+    "readOnly": true,
+    "value": 10.493096646942801,
+    "group": "Sum of comparable companies"
+}, {
+    "name": "Mortgage",
+    "show": true,
+    "striped": true,
+    "readOnly": true,
+    "value": 10.488958990536277,
+    "group": "Average of comparable companies"
+}, {
+    "name": "Mortgage",
+    "readOnly": true,
+    "show": true,
+    "striped": false,
+    "value": 15.37313432835821,
+    "group": "BANK OF AMERICA, NATIONAL ASSOCIATION"
+}, {
+    "name": "Debt collection",
+    "show": true,
+    "striped": false,
+    "readOnly": true,
+    "value": 11.321499013806706,
+    "group": "Sum of comparable companies"
+}, {
+    "name": "Debt collection",
+    "show": true,
+    "striped": true,
+    "readOnly": true,
+    "value": 9.053627760252365,
+    "group": "Average of comparable companies"
+}, {
+    "name": "Debt collection",
+    "readOnly": true,
+    "show": true,
+    "striped": false,
+    "value": 6.343283582089552,
+    "group": "BANK OF AMERICA, NATIONAL ASSOCIATION"
+}, {
+    "name": "Checking or savings account",
+    "show": true,
+    "striped": false,
+    "readOnly": true,
+    "value": 21.794871794871796,
+    "group": "Sum of comparable companies"
+}, {
+    "name": "Checking or savings account",
+    "show": true,
+    "striped": true,
+    "readOnly": true,
+    "value": 17.42902208201893,
+    "group": "Average of comparable companies"
+}, {
+    "name": "Checking or savings account",
+    "readOnly": true,
+    "show": true,
+    "striped": false,
+    "value": 34.32835820895522,
+    "group": "BANK OF AMERICA, NATIONAL ASSOCIATION"
+}, {
+    "name": "Credit reporting, credit repair services, or other personal consumer reports",
+    "show": true,
+    "striped": false,
+    "readOnly": true,
+    "value": 24.674556213017752,
+    "group": "Sum of comparable companies"
+}, {
+    "name": "Credit reporting, credit repair services, or other personal consumer reports",
+    "show": true,
+    "striped": true,
+    "readOnly": true,
+    "value": 19.73186119873817,
+    "group": "Average of comparable companies"
+}, {
+    "name": "Credit reporting, credit repair services, or other personal consumer reports",
+    "readOnly": true,
+    "show": true,
+    "striped": false,
+    "value": 13.059701492537313,
+    "group": "BANK OF AMERICA, NATIONAL ASSOCIATION"
+}, {
+    "name": "Credit card or prepaid card",
+    "show": true,
+    "striped": false,
+    "readOnly": true,
+    "value": 52.38658777120315,
+    "group": "Sum of comparable companies"
+}, {
+    "name": "Credit card or prepaid card",
+    "show": true,
+    "striped": true,
+    "readOnly": true,
+    "value": 41.89274447949527,
+    "group": "Average of comparable companies"
+}, {
+    "name": "Credit card or prepaid card",
+    "readOnly": true,
+    "show": true,
+    "striped": false,
+    "value": 25.671641791044774,
+    "group": "BANK OF AMERICA, NATIONAL ASSOCIATION"
+} ];
 
 function creategroupedRowChartWithTooltip(optionalColorSchema) {
     let groupedRow = groupedRowChart(),
@@ -155,7 +271,7 @@ function createHorizontalgroupedRowChart(optionalColorSchema) {
         dataset = testDataSet.with3Sources().build();
 
         // StackedAreChart Setup and start
-        const ratio = 16749/16480;
+        const ratio = 100/52.38658777120315;
         groupedRow
             .tooltipThreshold(600)
             .grid('vertical')
@@ -169,6 +285,7 @@ function createHorizontalgroupedRowChart(optionalColorSchema) {
                 right: 30,
                 bottom: 20
             })
+            .xTicks(10)
             // .nameLabel('date')
             // .valueLabel('views')
             // .groupLabel('stack')
