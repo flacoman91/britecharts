3745f71f03416cd785aac8712d4edc275357f038
Richard Dinh <richard.dinh@cfpb.gov>
2020-03-24 12:14:39 -0400

    excluding the files, and keeping karma-istanbul

--- karma.conf.js
+++ karma.conf.js
@@ -43,26 +43,82 @@ module.exports = function(config) {
 
         // Coverage reporter options, check more in:
         // https://github.com/karma-runner/karma-coverage
-        coverageReporter: {
-            type: 'text',
+
+        reporters: ['coverage-istanbul'],
+
+        // any of these options are valid: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-api/lib/config.js#L33-L39
+        coverageIstanbulReporter: {
+            // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
+            reports: ['html', 'lcovonly', 'text-summary'],
+
+            // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
             dir: 'stats/testCoverage/',
-            reporters: [
-                {type: 'text'},
-                {type: 'text-summary', subdir: '.', file: 'text-summary.txt'}
-            ],
-            check: {
+
+            // Combines coverage information from multiple browsers into one report rather than outputting a report
+            // for each browser.
+            combineBrowserReports: true,
+
+            // if using webpack and pre-loaders, work around webpack breaking the source path
+            fixWebpackSourcePaths: true,
+
+            // Omit files with no statements, no functions and no branches from the report
+            skipFilesWithNoCoverage: true,
+
+            // Most reporters accept additional config options. You can pass these through the `report-config` option
+            'report-config': {
+                // all options available at: https://github.com/istanbuljs/istanbuljs/blob/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib/html/index.js#L135-L137
+                html: {
+                    // outputs the report in ./coverage/html
+                    subdir: 'html'
+                }
+            },
+
+            // enforce percentage thresholds
+            // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
+            thresholds: {
+                emitWarning: true, // set to `true` to not fail the test command when thresholds are not met
+                // thresholds for all files
                 global: {
                     statements: 50,
+                    lines: 50,
+                    branches: 50,
+                    functions: 50
+                },
+                // thresholds per file
+                each: {
+                    statements: 50,
+                    lines: 50,
                     branches: 50,
                     functions: 50,
-                    lines: 50,
-                    excludes: [
-                        'tests_index.js',
-                        'src/charts/helpers/export.js',
-                        'src/charts/helpers/date.js',
-                        'src/charts/helpers/filter.js'
-                    ]
+                    overrides: {
+                        'src/charts/helpers/date.js': {
+                            statements: 0,
+                            lines: 0,
+                            branches: 0,
+                            functions: 0
+                        },
+                        'src/charts/helpers/export.js': {
+                            statements: 0,
+                            lines: 0,
+                            branches: 0,
+                            functions: 0
+                        },
+                        'src/charts/helpers/filter.js': {
+                            statements: 0,
+                            lines: 0,
+                            branches: 0,
+                            functions: 0
+                        }
+                    }
                 }
+            },
+
+            verbose: true, // output config used by istanbul for debugging
+
+            // `instrumentation` is used to configure Istanbul API package.
+            instrumentation: {
+                // To include `node_modules` code in the report.
+                'default-excludes': false
             }
         },
 
@@ -74,6 +130,7 @@ module.exports = function(config) {
 
         plugins: [
             require('karma-webpack'),
+            require('karma-coverage-istanbul-reporter'),
             require('karma-jasmine'),
             require('karma-jasmine-jquery'),
             require('karma-coverage'),
@@ -93,7 +150,6 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['dots', 'coverage'],
 
 
         // web server port
